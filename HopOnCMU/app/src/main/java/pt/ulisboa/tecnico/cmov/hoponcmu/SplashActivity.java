ef13c53ae24fcd4f69e0cf6be5305001ee1e688a
facas <joaotavaresfreitas@hotmail.com>
2018-03-17 02:08:46 +0000

    1.2 HopOnCMU App: Create SplashActivity and layout

---
+++ HopOnCMU/app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/SplashActivity.java
@@ -0,0 +1,25 @@
+package pt.ulisboa.tecnico.cmov.hoponcmu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent activityIntent;
+
+        // TODO: decide which activity to start
+        if (true) {
+            activityIntent = new Intent(this, LoginActivity.class);
+        } else {
+            activityIntent = new Intent(this, MainActivity.class);
+        }
+
+        startActivity(activityIntent);
+        finish();
+    }
+}
